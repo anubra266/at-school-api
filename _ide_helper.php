f0a7eb267018260084cd8a9a67be8ebacb905e72
root <root@server1.at-school.xyz>
2020-06-25 18:05:46 +0000

    configured laravel routing

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.11.0 on 2020-06-24 18:25:23.
+ * Generated for Laravel 7.11.0 on 2020-06-24 19:46:33.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
