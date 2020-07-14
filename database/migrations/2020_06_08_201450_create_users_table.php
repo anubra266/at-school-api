bf66a9b373057f62f214ca795a383f9f8f1ad9cf
Abraham <anubra266@gmail.com>
2020-07-14 12:28:27 +0100

    Add gender to users

--- database/migrations/2020_06_08_201450_create_users_table.php
+++ database/migrations/2020_06_08_201450_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('firstName');
             $table->string('middleName');
             $table->string('lastName');
-            $table->string('gender')->nullable();
+            $table->string('gender');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('telephone');
